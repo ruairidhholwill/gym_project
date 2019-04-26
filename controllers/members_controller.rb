f8c1ef20053644a14a7dbc8576a0ef7dc72443dd
Ruairidh Holwill <roore@hotmail.co.uk>
2019-04-26 17:12:25 +0100

    can now delete member and taken to an edit page for member

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -17,12 +17,23 @@ get '/members/new' do
   erb(:"members/new")
 end
 
+post '/members' do
+  Member.new(params).save
+  redirect to '/members'
+end
+
 get '/members/:id' do
-  @member = Member.new(params['id'])
+  @member = Member.find(params['id'])
   erb(:"members/show")
 end
 
-post '/members' do
-  Member.new(params).save
+get '/members/:id/edit' do
+  @member = Member.find(params['id'])
+  erb(:"members/edit")
+end
+
+post '/members/:id/delete' do
+  member = Member.find(params['id'])
+  member.delete()
   redirect to '/members'
 end
