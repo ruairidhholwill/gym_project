19edf508826fb6298ec74efb74f0747012638a5a
Ruairidh Holwill <roore@hotmail.co.uk>
2019-04-26 15:49:58 +0100

    members page created, individual member page created and new member form started

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -5,3 +5,18 @@ require_relative('../models/booking.rb')
 require_relative('../models/group_class.rb')
 require_relative('../models/member.rb')
 also_reload('../models/*')
+
+get '/members' do
+  @members = Member.all
+  @group_classes = GroupClass.all
+  erb(:"members/index")
+end
+
+get '/members/new' do
+  erb(:"members/new")
+end
+
+get '/members/:id' do
+  @member = Member.new(params['id'])
+  erb(:"members/show")
+end
