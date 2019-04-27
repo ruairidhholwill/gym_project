da7e11fd8c225986121a6c0ee2f2e950cf3b6ebe
Ruairidh Holwill <roore@hotmail.co.uk>
2019-04-27 11:29:34 +0100

    can see individual class and added new class form.

--- controllers/group_classes_controller.rb
+++ controllers/group_classes_controller.rb
@@ -13,7 +13,22 @@ get '/group_classes' do
   erb(:"group_classes/index")
 end
 
+get '/group_classes/new' do
+  erb(:"group_classes/new")
+end
+
+post '/group_classes' do
+  GroupClass.new(params).save
+  redirect to '/group_classes'
+end
+
 get '/group_classes/:id' do
-  @group_class = GroupClass.new(params['id'])
+  @group_class = GroupClass.find(params['id'])
   erb(:"group_classes/show")
+end
+
+post '/group_classes/:id/delete' do
+  group_class = GroupClass.find(params['id'])
+  group_class.delete()
+  redirect to '/group_classes'
 end
